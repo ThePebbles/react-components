d364c1551d0f73b8b5dba3ceb98d408fc4a516ea
Sarah <sarahkwando@gmail.com>
2022-12-13 09:58:18 -0600

    Complete React Components

--- app.js
+++ app.js
@@ -1,1 +1,65 @@
-// TODO
+//My code is below. It works, but bolds everything upon mouseover, not just the one item
+
+const GroceryList = () => (
+    <div>
+      <h1>My Grocery List</h1>
+      <GroceryListItems item={['Milk', 'Cookies', 'Brownies']}/>
+    </div>
+);
+
+function GroceryListItems(props) {
+  const listItems = props.item.map(item =>
+    <BoldItPlease item={item}/>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+function BoldItPlease(props) {
+  const { useState } = React;
+  const [isDone, setIsDone] = useState(false);
+  const style = {
+    fontWeight: isDone ? 'Bold' : 'normal'
+  };
+  return (
+    <li style={style} onMouseEnter={() => setIsDone(!isDone)} onMouseLeave={() => setIsDone(!isDone)}>
+      {props.item}
+    </li>
+  )
+
+}
+
+ReactDOM.render(<GroceryList/>, document.getElementById("app"));
+
+
+//The code below is copied from Learn. It does not work
+
+// const TodoList = (props) => (
+//   console.log('HI');
+//   return (
+//     <ul>
+//       {['Milk', 'Cookies', 'Brownies'].map((todo) => (
+//         <TodoListItem/>
+//       ))}
+//     </ul>
+//   )
+// );
+
+// const { useState } = React;
+
+// const TodoListItem = (props) => {
+//   const [isDone, setIsDone] = useState(false);
+//   const style = {
+//     fontWeight: isDone ? 'Bold' : 'none'
+//   };
+//   return (
+//     <h1>'HI'</h1>
+//     // <li style={style} onMouseOver={mouse()}>
+//     //   {props.todo}
+//     // </li>
+//   )
+// }
+
+
+
